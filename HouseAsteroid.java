158d634b0a1992665e7b8b4a7e685c6c6cade5e0
pretzel <132411345+presigues@users.noreply.github.com>
2024-10-18 12:54:19 -0400

    update new (dunno which number)

--- HouseAsteroid.java
+++ HouseAsteroid.java
@@ -15,83 +15,111 @@ import java.util.Random;
 import java.awt.event.*; 
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 
 /**
  *
  * @author pres2590
  */
-public class HouseAsteroid extends JPanel implements ActionListener, MouseListener{
+public class HouseAsteroid extends JPanel implements ActionListener{
     
-      Random rand = new Random();
-      int npose;
+      Random part = new Random();
       
-      int ballw=0;
-      int ballh=0;
-      int ballx=1000;
-      int bally=1000;
+      // EXPLOSION PARTICLE ------------------------------------
+   
+      int partspeedy1 = part.nextInt(10);
+      int partspeedx1 = part.nextInt(10);
+      int partspeedy2 = part.nextInt(10);
+      int partspeedx2 = part.nextInt(10);
+      
+      int partmove1;
+      int partmove2;
      
-      int a = rand.nextInt(10);
-      int b = rand.nextInt(10);
-         int xposa = 150;
-          int xposb = 300; 
+          
+        
+        
+          
+          // EXPLOSION -----------------------------------------
+          
+          int sizeY = 10;
+          int sizeX = 10;
+          
+          int size1;
+          int size2;
+          int size3;
+          int size4;
+          
+    // COLOR SHIFT -----------------------------------------------
+          int colora = 67;
+          int colorb= 185;
+          
+      
+    // TIMER ---------------------------
+     Timer time = new Timer(20, this);
      
-     Timer time = new Timer(5, this);
-        int x=0, y=0;
+     // Meteor Start Pos ----------------
+        int x=300, y=0;
         
-         @Override
-    public void mouseClicked(MouseEvent e) {
-      System.out.println("CLicked");
-    }
+  
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-       
-    }
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-       
-    }
-    boolean thefunstuff = false;
     
+   
+ 
+    // Start ------------------------------------------------------------
         public void actionPerformed(ActionEvent e){
         
+            if(x>=450&&size4<=2200){
                 
-            if(x > 100&&thefunstuff==false){
-                ballw= ballw+5;
-                ballh=ballh+5;
-                ballx= ballx-5;
-                bally=bally-5;
-                repaint();
-                thefunstuff=true;
-            }
-            if(x > 100&&thefunstuff==true){
-                ballw= ballw+5;
-                ballh=ballh+5;
-                 ballx= ballx-5;
-                bally=bally-5;
-                repaint();
-                thefunstuff=false;
-            }
+                if(colora<166){
+                colora=colora+2;
           
-        x += 10;
+                }
+                
+                if(colorb<200){
+                     colorb=colorb+2;
+                }
+                                 
+              size4+=35;
+          
+                repaint();
+                
+                if(size3<1600){
+                    size3+=15;
+                repaint();
+                
+                if(size2<900){
+                       size2+=10;
+                repaint();
+                
+                if(size1<400){
+                     size1+=6;
+                repaint();  
+                }
+                }
+                }
+                
+                
+                
+                
+             
+               
+           
+            }
+      
+          
+        x += 4;
 
-        y += 10;
+        y += 8;
         
-       ;
-         npose = npose++;
-        xposa = xposa + a; 
-        xposb = xposb + b; 
+        if(sizeY<200&&sizeX<200){
+        sizeY+=10;
+        sizeX+=10;
+        }
+        
+       
+        
 
         repaint();
         
@@ -99,15 +127,22 @@ public class HouseAsteroid extends JPanel implements ActionListener, MouseListen
         
         public void paint( Graphics g ) {
                          super.paintComponent(g);
+                         
+                      
                         
                        
-        
+          
                
             
         // CUSTOM COLORS ----------------------------------
         Color HouseBrown = new Color(162, 122, 71);
         Color Door = new Color(98, 78, 55);
-        Color Grass = new Color(67, 147, 93);
+          Color Sky = new Color(colorb, 253, 248); // create sky color
+        Color Grass = new Color(colora, 147, 93);
+          Color ex1 = new Color(245, 245, 161);
+          Color ex2 = new Color(255, 255, 40);
+          Color ex3 = new Color(255, 167, 40);
+         Color ex4 = new Color( 227, 84, 40);
         // ------------------------------------------------
                
             
@@ -117,11 +152,19 @@ public class HouseAsteroid extends JPanel implements ActionListener, MouseListen
           int npos = 3;
           // ------------------------------------------
           
+             // ASTEROID POS -----------------------------------      
+          int[] xposa = {10+x, 100+x, 200+x,300+x, 400+x, 40+x, 60+x};
+          int[] yposa = {100+y, 150+y,180+y, 200+y, 10+y, -100+y, -150+y};
+          int nposa = 7;
+        
+          // ------------------------------------------
+          
        
           
-            int[] xposas = {x, y+10, xposa};
-          int[] yposas = {x+10, x, xposb};
-          int nposas = 3;
+         g.setColor(Sky);
+            g.fillRect(0,0,2000,2000);
+       
+        
                 
         // draw door 
           g.setColor(Door);
@@ -147,9 +190,7 @@ public class HouseAsteroid extends JPanel implements ActionListener, MouseListen
         g.setColor(Door); 
         g.fillRect(1025, 600, 250, 200);
         
-        // draw grass
-        g.setColor(Grass);
-        g.fillRect(0, 800, 2000, 200);
+       
         
         // draw driveway
         g.setColor(Color.gray);
@@ -183,29 +224,58 @@ public class HouseAsteroid extends JPanel implements ActionListener, MouseListen
         g.fillOval(1600, 200, 100, 100);               
         g.fillOval(1650, 250, 100, 100);       
         g.fillOval(1700, 300, 100, 100);
+        
+        
+       
+         g.setColor(Color.GRAY);
+           
+    //g.fillOval(x, y, sizeY, sizeX);
+        g.fillPolygon(xposa, yposa, nposa);
+        
                 
-             g.setColor(Color.GRAY);
-        g.fillOval(x, y, 100, 100);
-         g.fillPolygon(xposas, yposas, nposas);
-         
-          g.setColor(Color.yellow);
-        g.fillOval(ballx, bally, ballw, ballh);
+          // draw grass
+        g.setColor(Grass);
+        g.fillRect(0, 800, 2000, 200);
+        
+        g.setColor(ex4);
+         g.fillOval(700- size4/2,400- size4/2, size4, size4);
+              g.setColor(ex3);
+         g.fillOval(700- size3/2,400- size3/2, size3, size3);
+          g.setColor(ex2);
+         g.fillOval(700- size2/2,400- size2/2, size2, size2);       
+          g.setColor(ex2);
+         g.fillOval(700- size2/2,400- size2/2, size2, size2);
+       
+       g.setColor(ex1);
+        g.fillOval(700- size1/2,400- size1/2, size1, size1); 
+        
+        g.setColor(Color.black);
+        g.fillOval(700, 500, 0, 0);
+         g.fillOval(700, 400, 0, 0);
+          g.fillOval(500, 450, 0, 0);
+        
+        
+        
+        
             time.start();
+            
+            
+            
         }
 
      
       
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         
-            Color Sky = new Color(196, 240, 254); // create sky color
+          
         
       JFrame f = new JFrame("House"); // name frame
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set program to exit on close
         f.getContentPane().add(new HouseAsteroid()); // add graphics stuff to frame
         f.setSize(1900, 1000); // set frame size on start
         f.setLocation(0, 0); // set frame location on start
-        f.setBackground(Sky);
+       
     
   
         f.setVisible(true);
